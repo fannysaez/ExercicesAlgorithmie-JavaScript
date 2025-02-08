a8fa9071078d56d0beaee1fce7ff8463f89b5432
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-08 18:02:06 +0100

    initiale

---
+++ exercices/0.1/script0.1.js
@@ -0,0 +1,10 @@
+let number1 = 36;
+let number2 = 36;
+
+if (number1 < number2) {
+  console.log(number1);
+} else if (number1 > number2) {
+  console.log(number2);
+} else {
+  console.log("C'est les mêmes !!");
+}
