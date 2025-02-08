548b69de59c5adf2604ced05d507c52fe3bbdf5f
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-08 18:46:33 +0100

    exercice 0.3

---
+++ exercices/0.3/script0.3.js
@@ -0,0 +1,5 @@
+let gender = "une femme";
+let height = "160";
+let age = 38;
+
+console.log(" J'ai " + " " + age + " " + " ans" + "," + " "+ "je mesure" + " " + height + " " + " centimètres et je suis " + " " + gender )
