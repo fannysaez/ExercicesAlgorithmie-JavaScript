4fc0cd149a064696a1b251e30472131a84a524fb
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-08 21:21:58 +0100

    exercice 0.4

---
+++ exercices/0.4/script0.4.js
@@ -0,0 +1,15 @@
+
+//1- Déclaré deux variables, c'est à dire ! une variable age et une variable height (mesure)
+let age1 = 38;
+let height1 = "160";
+
+//2- Condition qui permet de vérifier :
+if(age1 > 18 && height1 > 140){
+    console.log("Je peux rentrer dans le manège");
+}
+else {
+    console.log("Je n'ai pas le droit de rentrer dans le manège");
+}
+//L'opérateur ET logique (&&) renvoie vrai si et uniquement si 
+// ses deux opérandes sont true ou équivalents à true.
+
