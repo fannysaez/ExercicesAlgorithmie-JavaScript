1a58f45491c6247c15dde5ca622bf9e3bd83e509
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-09 06:51:26 +0100

    exercice 0.3

--- exercices/0.3/script0.3.js
+++ exercices/0.3/script0.3.js
@@ -1,5 +1,13 @@
-let gender = "une femme";
-let height = "160";
+let gender = "male";
+let height = 175;
 let age = 38;
 
-console.log(" J'ai " + " " + age + " " + " ans" + "," + " "+ "je mesure" + " " + height + " " + " centimètres et je suis " + " " + gender )
+if (gender === "male") {
+    console.log(
+      `J'ai ${age} ans, je mesure ${height} centimètres et je suis un homme.`
+    );
+  } else if (gender === "female") {
+    console.log(
+      `J'ai ${age} ans, je mesure ${height} centimètres et je suis une femme.`
+    );
+  }
